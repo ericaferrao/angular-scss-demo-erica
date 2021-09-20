3e420e25e72f97246cb5b7f9f0d112b3994f9393
ericaferrao <ferraoerica@gmail.com>
2021-09-20 13:22:35 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NewComponent } from './new/new.component';
-import { GoatComponent } from './goat/goat.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NewComponent,GoatComponent ],
+  declarations: [ AppComponent, HelloComponent, NewComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
